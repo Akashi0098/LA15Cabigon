73b2ac7ec222e440bc1560d4111a6f950f8e1b1c
Akashi0098 <132603672+Akashi0098@users.noreply.github.com>
2024-12-06 22:59:03 +0800

    initial commit

--- blog_projCabigon/blog_appCabigon/views.py
+++ blog_projCabigon/blog_appCabigon/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def say_hello(request):
+    return HttpResponse("Hello Django")
+
+def say_hi(request):
+    return render(request, "hi.html", {"name": "Alwynn Ronan C. Cabigon"})
+
+def blog_list(request):
+    post = Post.objects.all()
+    context = {
+        'blog_list': post
+    }
+
+    return render(request, "blog_list.html", context)
+
+def blog_detail(request, id):
+    post = Post.objects.get(id=id)
+    context = {
+        'blog_detail': post
+    }
+
+    return render(request, "blog_detail.html", context)
+
